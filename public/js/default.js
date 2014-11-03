8ff5d83503ee74573c05b50401b4810cc0655245
Zhen Zheng <zxzheng@ucsd.edu>
2014-11-03 15:05:58 -0800

    Tracking fixed

--- public/js/default.js
+++ public/js/default.js
@@ -3,3 +3,22 @@
 window.setTimeout(function(){
 	$('header').removeClass("hideHeader").addClass("showHeader");
 },700);
+
+if (mypages[myindex] == "projects"){
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-48511938-4', 'zxzdesigns.com/projects');
+    ga('send', 'pageview');
+}
+else{
+	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-48511938-4', 'zxzdesigns.com/projects'+mypages[myindex]);
+    ga('send', 'pageview');
+}
--- public/js/home.js
+++ public/js/home.js
@@ -2,3 +2,11 @@
 // var titleWidth = $('#splashHome .splashTitle h2').width();
 // $('#splashHome .splashTitle .line').css({"width": titleWidth});
 layoutResize();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-48511938-4', 'zxzdesigns.com/home');
+ga('send', 'pageview');
