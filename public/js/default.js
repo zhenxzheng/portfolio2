7ad84d9efa9e138e3f2276adb88fb080caae242a
Zhen Zheng <zxzheng@ucsd.edu>
2014-11-03 16:31:51 -0800

    Analytic page extention fix attemp

--- public/js/default.js
+++ public/js/default.js
@@ -11,7 +11,10 @@ if (mypages[myindex] == "projects"){
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
     ga('create', 'UA-48511938-4', 'zxzdesigns.com/projects');
-    ga('send', 'pageview');
+    ga('send', {
+	    'hitType':'pageview',
+	    'page':'/projects'
+  });
 }
 else{
 	    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -19,6 +22,9 @@ else{
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-48511938-4', 'zxzdesigns.com/projects'+mypages[myindex]);
-    ga('send', 'pageview');
+    ga('create', 'UA-48511938-4', 'zxzdesigns.com/projects/'+mypages[myindex]);
+    ga('send', {
+	    'hitType':'pageview',
+	    'page':'/projects/'+mypages[myindex]
+  });
 }
