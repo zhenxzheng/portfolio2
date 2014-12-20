31a1aaaff2bb7012a35add87aa92cf8079a71869
Zhen Zheng <zxzheng@ucsd.edu>
2014-12-19 16:45:07 -0800

    fix tracking code attempt1

--- public/js/home.js
+++ public/js/home.js
@@ -10,4 +10,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-48511938-4', 'zxzdesigns.com/home');
-ga('send', 'pageview');
+ga('send', {
+    'hitType':'pageview',
+    'page':'/home'
+});
